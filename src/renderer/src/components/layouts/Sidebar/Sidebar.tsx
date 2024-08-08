d8008fc7137ad6dfc8eb24c07361d6046424d96c
Gorden <60036275+gorden0929@users.noreply.github.com>
2024-08-08 16:32:34 +0800

    update personal information

--- src/renderer/src/components/layouts/Sidebar/Sidebar.tsx
+++ src/renderer/src/components/layouts/Sidebar/Sidebar.tsx
@@ -3,6 +3,8 @@ import styles from './Sidebar.module.scss'
 import Icon from '@renderer/components/widgets/Icon/Icon'
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
+import SampleProfilePic from '@renderer/assets/images/sample-profile-pic.jpg'
+import { Avatar } from 'antd'
 
 type Props = {}
 
@@ -12,7 +14,7 @@ export default function Sidebar({}: Props) {
     <div className={styles.sidebarWrapper}>
       <div className={styles.sidebarUpperSection}>
         <div className={styles.avatarWrapper}>
-          <img src="https://github.com/shadcn.png" className={styles.avatar} />
+          <Avatar src={SampleProfilePic} className={styles.avatar} />
         </div>
         <div className={styles.iconWrapper}>
           <Icon name="chat" />
--- src/renderer/src/pages/Settings/PersonalInformation/PersonalInformation.tsx
+++ src/renderer/src/pages/Settings/PersonalInformation/PersonalInformation.tsx
@@ -1,39 +1,159 @@
-import styles from './PersonalInformation.module.scss'
+import Icon from '@renderer/components/widgets/Icon/Icon'
 import '@renderer/styles/settings.scss'
+import { Avatar, Button, Input, Modal, Radio, RadioChangeEvent } from 'antd'
+import { useState } from 'react'
+import SampleProfilePic from '@renderer/assets/images/sample-profile-pic.jpg'
 
 export default function PersonalInformation() {
-  return (
-    <div className={styles.personalInfomationWrapper}>
-      <div className={styles.cardWrapper}>
-        <div className={styles.cardWrapper}>
-          <p>Mr Meow Meow</p>
-        </div>
-      </div>
+  const [value, setValue] = useState('male')
+  const [isProfilePictureModalOpen, setIsProfilePictureModalOpen] = useState(false)
+  const [isIdModalOpen, setIsIdModalOpen] = useState(false)
+  const [newProfilePic, setNewProfilePic] = useState<File>()
+  const [newProfilePicUrl, setNewProfilePicUrl] = useState<string>()
+  const [isSignatureModalOpen, setIsSignatureModalOpen] = useState(false)
+  const [signature, setSignature] = useState('')
 
-      <div className={styles.personalWrapper}>
-        <div className={styles.rowWrapper}>
-          <div>FFSAY ID</div>
-          <div className="flex flex-row">
-            <div> JX041914</div>
+  const onChange = (e: RadioChangeEvent) => {
+    console.log('radio checked', e.target.value)
+    setValue(e.target.value)
+  }
+  return (
+    <>
+      <div className="settingsCardList">
+        <div className="settingsCard">
+          <div className="settingsCardItem">
+            <div
+              style={{ display: 'flex', alignItems: 'center', gap: '1rem', position: 'relative' }}
+            >
+              <label htmlFor="avatarInput">
+                <Avatar size={56} icon={<img src={SampleProfilePic} />} />
+              </label>
+              <Input
+                type="file"
+                style={{ visibility: 'hidden', position: 'absolute' }}
+                id="avatarInput"
+                accept="image/*"
+                onChange={(e) => {
+                  setNewProfilePic(e.target.files![0])
+                  setNewProfilePicUrl(URL.createObjectURL(e.target.files![0]))
+                  setIsProfilePictureModalOpen(true)
+                }}
+              />
+              <h4>name</h4>
+            </div>
           </div>
         </div>
-
-        <hr className="bg-black w-full" />
-
-        <hr className="bg-black w-full" />
-
-        <div className={styles.rowWrapper}>
-          <div>Gender</div>
-          <div className="flex flex-row"></div>
-        </div>
-
-        <hr className="bg-black w-full" />
-
-        <div className={styles.rowWrapper}>
-          <div>Personal Signature</div>
-          <div className="flex flex-row"></div>
+        <div className="settingsCard">
+          <div className="settingsCardItem" onClick={() => setIsIdModalOpen(true)}>
+            <h4>ID</h4>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+              1234
+              <Icon name="chevron_right" weight={200} />
+            </div>
+          </div>
+          <div className="settingsCardItem">
+            <h4>我的二维码</h4>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+              <Icon name="qr_code" weight={200} />
+              <Icon name="chevron_right" weight={200} />
+            </div>
+          </div>
+          <div className="settingsCardItem">
+            <h4>性别</h4>
+            <Radio.Group onChange={onChange} value={value} style={{ display: 'flex', gap: '1rem' }}>
+              <Radio value="male">男</Radio>
+              <Radio value="female">女</Radio>
+            </Radio.Group>
+          </div>
+          <div className="settingsCardItem" onClick={() => setIsSignatureModalOpen(true)}>
+            <h4>个性签名</h4>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+              <p>{signature}</p>
+              <Icon name="chevron_right" weight={200} />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <Modal
+        styles={{
+          content: { padding: 0 },
+          header: { padding: '16px 24px', margin: 0 }
+        }}
+        title="设置头像"
+        open={isProfilePictureModalOpen}
+        onOk={() => {
+          setIsProfilePictureModalOpen(false)
+        }}
+        onCancel={() => {
+          setIsProfilePictureModalOpen(false)
+        }}
+        footer={
+          <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '1rem' }}>
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '1rem' }}>
+              <Button
+                onClick={() => {
+                  setIsProfilePictureModalOpen(false)
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="primary"
+                onClick={() => {
+                  setIsProfilePictureModalOpen(false)
+                }}
+              >
+                Confirm
+              </Button>
+            </div>
+          </div>
+        }
+        width={350}
+        centered
+      >
+        <img src={newProfilePicUrl} style={{ width: '100%' }} />
+      </Modal>
+      <Modal
+        title="ID"
+        open={isIdModalOpen}
+        onOk={() => {
+          setIsIdModalOpen(false)
+        }}
+        onCancel={() => {
+          setIsIdModalOpen(false)
+        }}
+        centered
+      >
+        您的ID是 ABCD1234
+      </Modal>
+      <Modal
+        title="个性签名"
+        open={isSignatureModalOpen}
+        footer={
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '1rem' }}>
+              <Button
+                onClick={() => {
+                  setIsSignatureModalOpen(false)
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="primary"
+                onClick={() => {
+                  setIsSignatureModalOpen(false)
+                }}
+              >
+                Confirm
+              </Button>
+            </div>
+          </div>
+        }
+        centered
+      >
+        <Input.TextArea rows={4} value={signature} onChange={(e) => setSignature(e.target.value)} />
+      </Modal>
+    </>
   )
 }
