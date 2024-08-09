a0f2448b672c4ef9f9cf09f5a8a87b90a0b74e45
Gorden <60036275+gorden0929@users.noreply.github.com>
2024-08-09 11:04:50 +0800

    update 404

--- src/renderer/src/pages/Settings/Settings.tsx
+++ src/renderer/src/pages/Settings/Settings.tsx
@@ -1,9 +1,14 @@
 import SettingsSidebar from '@renderer/components/layouts/SettingsSidebar/SettingsSidebar'
 import '@renderer/styles/settings.scss'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigate } from 'react-router-dom'
 import styles from './Settings.module.scss'
+import { useEffect } from 'react'
 
 export default function Settings() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate('/settings/personal-information')
+  }, [])
   return (
     <div className={styles.settingsWrapper}>
       <SettingsSidebar />
