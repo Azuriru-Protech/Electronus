ac8fd6560592ea5b0f78115392611db565c07ba0
KJ <kjtee@prosdtech.com>
2024-08-22 11:04:06 +0800

    group settings drawer UI

--- src/renderer/src/components/widgets/GroupSettingsEditDrawer/GroupSettingsEditDrawer.tsx
+++ src/renderer/src/components/widgets/GroupSettingsEditDrawer/GroupSettingsEditDrawer.tsx
@@ -3,6 +3,7 @@ import ChangeProfilePictureModal from '../ChangeProfilePictureModal/ChangeProfil
 import { Divider, Drawer, Switch } from 'antd'
 import SettingsCardItem from '../SettingsCardItem/SettingsCardItem'
 import sampleProfilePic from '../../../assets/images/sample-profile-pic.jpg'
+import Separator from '../Separator/Separator'
 
 type Props = {
   isOpen: boolean
@@ -12,8 +13,8 @@ type Props = {
 export default function GroupSettingsEditDrawer({ isOpen, setIsOpen }: Props) {
   const [isProfilePictureModalOpen, setIsProfilePictureModalOpen] = useState(false)
   const [newProfilePicUrl, setNewProfilePicUrl] = useState<string>()
-  const [groupEntryVerfication, setGroupEntryVerfication] = useState(false)
-  const [screenCaptureProhibitied, setScreenCaptureProhibitied] = useState(false)
+  const [groupEntryVerification, setGroupEntryVerification] = useState(false)
+  const [screenCaptureProhibited, setScreenCaptureProhibited] = useState(false)
   const [hideNotifications, setHideNotifications] = useState(false)
   const [groupMembersMute, setGroupMembersMute] = useState(false)
   const [groupMembersAreInvisible, setGroupMembersAreInvisible] = useState(false)
@@ -25,6 +26,78 @@ export default function GroupSettingsEditDrawer({ isOpen, setIsOpen }: Props) {
   const [groupMembersCantModifyName, setGroupMembersCantModifyName] = useState(false)
   const [groupMembersCantAddMembers, setGroupMembersCantAddMembers] = useState(false)
   const [reminderManagement, setReminderManagement] = useState(false)
+
+  const settingsData = [
+    {
+      title: 'Group Entry Verification',
+      description:
+        'After enabling, enter the group through group links or invitations need to be approved ...',
+      state: groupEntryVerification,
+      toggleAction: () => setGroupEntryVerification(!groupEntryVerification)
+    },
+    {
+      title: 'Screen Capture Prohibited',
+      state: screenCaptureProhibited,
+      toggleAction: () => setScreenCaptureProhibited(!screenCaptureProhibited)
+    },
+    {
+      title: 'Hide Notifications',
+      description:
+        'After enabling, ordinary users will not receive notification on users that are muted, removed or left the group ...',
+      state: hideNotifications,
+      toggleAction: () => setHideNotifications(!hideNotifications)
+    },
+    {
+      title: 'Group Members Mute',
+      state: groupMembersMute,
+      toggleAction: () => setGroupMembersMute(!groupMembersMute)
+    },
+    {
+      title: 'Group Members Are Invisible',
+      state: groupMembersAreInvisible,
+      toggleAction: () => setGroupMembersAreInvisible(!groupMembersAreInvisible)
+    },
+    {
+      title: "Group Members Can't Send Name Card",
+      state: groupMembersSendNameCards,
+      toggleAction: () => setGroupMembersSendNameCards(!groupMembersSendNameCards)
+    },
+    {
+      title: 'Group Members Are Forbidden to Send Pictures',
+      state: groupMembersForbiddenSendPic,
+      toggleAction: () => setGroupMembersForbiddenSendPic(!groupMembersForbiddenSendPic)
+    },
+    {
+      title: 'Group Members Are Forbidden to Send Videos',
+      state: groupMemberForbiddenSendVid,
+      toggleAction: () => setGroupMemberForbiddenSendVid(!groupMemberForbiddenSendVid)
+    },
+    {
+      title: 'Group Members Are Forbidden to Send Files',
+      state: groupMembersForbiddenSendFile,
+      toggleAction: () => setGroupMembersForbiddenSendFile(!groupMembersForbiddenSendFile)
+    },
+    {
+      title: 'Group Members List Is Not Visible',
+      state: groupMembersListNotVisible,
+      toggleAction: () => setGroupMembersListNotVisible(!groupMembersListNotVisible)
+    },
+    {
+      title: 'Group Members Cant Modify Name',
+      state: groupMembersCantModifyName,
+      toggleAction: () => setGroupMembersCantModifyName(!groupMembersCantModifyName)
+    },
+    {
+      title: 'Group Members Cant Add Members',
+      state: groupMembersCantAddMembers,
+      toggleAction: () => setGroupMembersCantAddMembers(!groupMembersCantAddMembers)
+    },
+    {
+      title: 'Reminder Management',
+      state: reminderManagement,
+      toggleAction: () => setReminderManagement(!reminderManagement)
+    }
+  ]
 
   return (
     <Drawer
@@ -39,140 +112,74 @@ export default function GroupSettingsEditDrawer({ isOpen, setIsOpen }: Props) {
       open={isOpen}
       styles={{ body: { padding: 0, width: '100%' } }}
     >
-      <ChangeProfilePictureModal
-        isOpen={isProfilePictureModalOpen}
-        setIsOpen={setIsProfilePictureModalOpen}
-        imageUrl={newProfilePicUrl!}
-      ></ChangeProfilePictureModal>
-      <SettingsCardItem
-        avatar={sampleProfilePic}
-        secondaryTitle="Set New Profile Picture"
-        onClick={() => setIsProfilePictureModalOpen(true)}
-      />
-      <Divider style={{ margin: 0 }} />
-      <SettingsCardItem title={'Group Name'} content={'HOTDOG'} />
-      <Divider style={{ margin: 0 }} />
-      <SettingsCardItem
-        title={'Group Announcement'}
-        content={'123 this is group announcement'}
-        onClick={() => setIsProfilePictureModalOpen(true)}
-      />
-      <Divider style={{ margin: 0 }} />
       <div className="settingsCardList">
-        <div className="settingsCard">
-          <div className="settingsCardItemBorderless">
-            <h4>Group Entry Verification</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupEntryVerfication(!groupEntryVerfication)}
-              size="small"
-            />
+        <ChangeProfilePictureModal
+          isOpen={isProfilePictureModalOpen}
+          setIsOpen={setIsProfilePictureModalOpen}
+          imageUrl={newProfilePicUrl!}
+        ></ChangeProfilePictureModal>
+        <SettingsCardItem
+          avatar={sampleProfilePic}
+          secondaryTitle="Set New Profile Picture"
+          onClick={() => setIsProfilePictureModalOpen(true)}
+        />
+        <Divider style={{ margin: 0 }} />
+        <SettingsCardItem title={'Group Name'} content={'HOTDOG'} />
+        <Divider style={{ margin: 0 }} />
+        <SettingsCardItem
+          title={'Group Announcement'}
+          content={'123 this is group announcement'}
+          onClick={() => setIsProfilePictureModalOpen(true)}
+        />
+        <Divider style={{ margin: 0 }} />
+      </div>
+
+      <div className="settingsCardList">
+        {settingsData.map((setting, index) => (
+          <div className="settingsCardItemBorderlessWrapper" key={index}>
+            <div className="contentContainer">
+              <h4>{setting.title}</h4>
+              {setting.description && <p>{setting.description}</p>}
+            </div>
+            <div className="actionContainer">
+              <Switch
+                defaultChecked={setting.state}
+                onChange={setting.toggleAction}
+                size="default"
+              />
+            </div>
           </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Screen Capture Prohibited</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setScreenCaptureProhibitied(!screenCaptureProhibitied)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Hide Notifications</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setHideNotifications(!hideNotifications)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Group Members Mute</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupMembersMute(!groupMembersMute)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Group Members Are Invinsible</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupMembersAreInvisible(!groupMembersAreInvisible)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Group Members Can't Send Name Card</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupMembersSendNameCards(!groupMembersSendNameCards)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Group Members Are Forbidden to Send Pictures</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupMembersForbiddenSendPic(!groupMembersForbiddenSendPic)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Group Members Are Forbidden to Send Videos</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupMemberForbiddenSendVid(!groupMemberForbiddenSendVid)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Group Members Are Forbidden to Send Videos</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupMemberForbiddenSendVid(!groupMemberForbiddenSendVid)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Group Members Are Forbidden to Send Files</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupMembersForbiddenSendFile(!groupMembersForbiddenSendFile)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Group Members List Is Not Visible</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupMembersListNotVisible(!groupMembersListNotVisible)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Group Members Cant Modify Name</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupMembersCantModifyName(!groupMembersCantModifyName)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Group Members Cant Add Members</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setGroupMembersCantAddMembers(!groupMembersCantAddMembers)}
-              size="small"
-            />
-          </div>
-          <div className="settingsCardItemBorderless">
-            <h4>Reminder Management</h4>
-            <Switch
-              defaultChecked
-              onChange={() => setReminderManagement(!reminderManagement)}
-              size="small"
-            />
-          </div>
-        </div>
+        ))}
+      </div>
+
+      <div className="settingsCardList">
+        <Divider style={{ margin: 0 }} />
+        <SettingsCardItem
+          title={'Chat History'}
+          onClick={() => setIsProfilePictureModalOpen(true)}
+        />
+        <SettingsCardItem
+          title={'Special Remarks'}
+          onClick={() => setIsProfilePictureModalOpen(true)}
+        />
+        <SettingsCardItem
+          title={'Special Restrictions'}
+          onClick={() => setIsProfilePictureModalOpen(true)}
+        />
+        <SettingsCardItem
+          title={'Mange Inactive Users'}
+          onClick={() => setIsProfilePictureModalOpen(true)}
+        />
+        <Divider style={{ margin: 0 }} />
+        <SettingsCardItem
+          title={'Chat Sensitive Words'}
+          onClick={() => setIsProfilePictureModalOpen(true)}
+        />
+        <Divider style={{ margin: 0 }} />
+        <SettingsCardItem
+          title={'Manage Logs'}
+          onClick={() => setIsProfilePictureModalOpen(true)}
+        />
       </div>
     </Drawer>
   )
